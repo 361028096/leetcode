850102da6a718843bd7c22ace03bb6a16795843d
CyberLion369 <361028096@qq.com>
2016-09-30 11:57:55 +0800

    20160930
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei20160930
    
    hewei

--- src/D20160930N136SingleNumber.java
+++ src/D20160930N136SingleNumber.java
@@ -1,21 +1,25 @@
+/*
+
+*1.for的遍历
+*2.注意数组长度for(int i=0;i<nums.length;i++)
+*
+*/
 
 public class D20160930N136SingleNumber {
-	public static void main(String args[]){
-		int[] nums = {1,1,2,2,3};
+	public static void main(String args[]) {
+		int[] nums = { 1, 1, 2, 2, 3 };
 		System.err.println(nums.length);
-		D20160930N136SingleNumber a =new D20160930N136SingleNumber();
+		D20160930N136SingleNumber a = new D20160930N136SingleNumber();
 		System.out.println(a.singleNumber(nums));
-		
 	}
-	
+
 	int singleNumber(int[] nums) {
 		int result = 0;
-		for(int i:nums)
-			result ^=i;
-		/*for(int i=0;i<nums.length;i++)
-			result ^=nums[i];*/
+		for (int i : nums)
+			result ^= i;
+		/*
+		 * for(int i=0;i<nums.length;i++) result ^=nums[i];
+		 */
 		return result;
 	}
-
-
 }
