7e8763a2f3298aa3e1c66d4c3dcb8447480339de
CyberLion369 <361028096@qq.com>
2017-05-12 14:08:02 +0800

    update

---
+++ src/D20170512N461HammingDistance.java
@@ -0,0 +1,13 @@
+
+public class D20170512N461HammingDistance {
+	public int hummingDistance(int x, int y) {
+		int count = 0;
+		int xor = x ^ y;
+		// 计算1的个数
+		while (xor != 0) {
+			xor &= xor-1;
+			count++;
+		}
+		return count;
+	}
+}
