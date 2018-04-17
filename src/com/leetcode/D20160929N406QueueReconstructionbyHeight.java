71a4b71a07e8d7b204c955998eb4e54adef73d3a
wei.he <wei.he@bkjk.com>
2018-04-17 21:52:36 +0800

    update

--- src/com/leetcode/D20160929N406QueueReconstructionbyHeight.java
+++
@@ -1,8 +0,0 @@
-package com.leetcode;
-
-public class D20160929N406QueueReconstructionbyHeight {
-
-	/*public int[][] reconstructQueue(int[][] people) {
-
-	}*/
-}
---
+++ src/test/java/test.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+/**
+ * @author : hewei
+ * @description: TODO
+ * @date : 2018/4/17
+ */
+public class test {
+    @Test
+    public void testString() {
+        String s = "aaaaa";
+        System.out.println(s);
+//        for(char c: s) {
+//            System.out.println(c);
+//        }
+    }
+
+}
