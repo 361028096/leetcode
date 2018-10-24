740c15fae5a71c437ae14e353ef8f7261f0d4ad1
wei.he <wei.he@bkjk.com>
2018-10-24 15:19:25 +0800

    update

---
+++ src/main/java/com/leetcode/D20180731N006ZigZagConversion.java
@@ -0,0 +1,40 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+ *
+ * @author hewei
+ * @date 2018/7/31
+ */
+public class D20180731N006ZigZagConversion {
+    public String convert(String s, int numRows) {
+
+//        if (numRows == 1) {
+//            return s;
+//        }
+//
+//        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+//        for (int i = 0; i < Math.min(numRows, s.length()); i++) {
+//            rows.add(new StringBuilder());
+//        }
+//        int curRow = 0;
+//        boolean goingDown = false;
+//
+//        for (char c : s.toCharArray()) {
+//            rows.get(curRow).append(c);
+//            if (curRow == 0 || curRow == numRows - 1) {
+//                goingDown = =! goingDown;
+//            }
+//        }
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+//        this.convert("aaaa",1);
+        System.out.println("aaa");
+    }
+}
