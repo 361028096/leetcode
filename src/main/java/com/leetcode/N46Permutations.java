6988368f1b88137374439a00b085141d890021ba
wei.he <wei.he@bkjk.com>
2019-04-21 12:00:23 +0800

    update

--- src/main/java/com/leetcode/N46Permutations.java
+++ src/main/java/com/leetcode/N46Permutations.java
@@ -1,9 +1,20 @@
 package com.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author hewei
  * @description TODO
  * @date 2019/3/19 10:56
  */
 public class N46Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> resultList = new ArrayList<>();
+        for (int i=0;i<nums.length;i++) {
+            List<Integer> singalResultList = new ArrayList<>();
+            singalResultList.add(nums[i]);
+        }
+        return null;
+    }
 }
