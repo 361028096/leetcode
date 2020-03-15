57e640cae2eb904cf71a9d4971772fc156e91b53
hewei <361028096@qq.com>
2020-03-16 00:11:41 +0800

    update code

---
+++ src/main/java/com/leetcode/test.java
@@ -0,0 +1,13 @@
+package com.leetcode;
+
+/**
+ * TODO
+ *
+ * @author hewei
+ * @date 2020/3/15 20:41
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.printf("hello world");
+    }
+}
